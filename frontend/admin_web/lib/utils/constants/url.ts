e41860c15922e1148c90fb0e1d129b889b44250c
Suren Bandara <suren.bandara@yaalalabs.com>
2025-03-16 10:41:42 +0530

    Add local url

--- frontend/admin_web/lib/utils/constants/url.ts
+++ frontend/admin_web/lib/utils/constants/url.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL = {
   LOCAL: {
-    SERVER_URL: 'http://192.168.0.131:8001/', 
+    SERVER_URL: 'http://localhost:3000', 
     WS_SERVER_URL: 'ws://192.168.0.131:8001/', 
   },
   LIVE: {
