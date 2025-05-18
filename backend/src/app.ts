795b3dbf22957d56099d161247c95b358be28795
Suren Bandara <suren.bandara@yaalalabs.com>
2025-05-18 21:59:30 +0530

    Enable request validation in OpenApiValidator middleware and reorder middleware usage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -35,13 +35,13 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
 app.use(
     OpenApiValidator.middleware({
         apiSpec,
-        validateRequests: false,
+        validateRequests: true,
         validateResponses: true,
     })
 );
 starterPack();
 app.use(authValidation);
-app.use(operationHandler);
 app.use(apiValidationHandler);
+app.use(operationHandler);
 
 export default app;
